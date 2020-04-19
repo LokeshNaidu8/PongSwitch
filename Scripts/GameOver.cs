7ff4214f73892280f96e97b3564761b1ef1c1d99
unknown <lokeshwaran8799@gmail.com>
2020-04-19 13:56:15 +0530

    first commit

---
+++ Scripts/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public void GoToMenu()
+    {
+       
+        SceneManager.LoadScene(0);
+    }
+
+    public void Retry()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
